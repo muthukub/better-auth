128cb7774f542bfe6dc02e353c4b35b348b94b5c
Kinfe123 <kinfishtech@gmail.com>
2025-05-30 12:07:54 -0700

    tsconfig include

--- demo/nextjs/global.d.ts
+++ demo/nextjs/global.d.ts
@@ -8,3 +8,15 @@ declare module '@radix-ui/react-tabs' {
   export interface TabsTriggerProps extends PropsWithChildren {}
   export interface TabsContentProps extends PropsWithChildren {}
 }
+declare module "@radix-ui/react-radio-group" {
+    export interface RadioGroupItemProps extends ComponentProps<ReactElement> {}
+    export const Item: React.ForwardRefExoticComponent<
+      RadioGroupItemProps & React.RefAttributes<HTMLButtonElement>
+    >;
+  
+    export interface RadioGroupIndicatorProps
+      extends ComponentProps<ReactElement> {}
+    export const Indicator: React.ForwardRefExoticComponent<
+      RadioGroupIndicatorProps & React.RefAttributes<HTMLSpanElement>
+    >;
+  }
